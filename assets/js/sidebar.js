3fdd91c385785bca4bc6486d13f8e600dde07c55
Nam Nguyễn <nam278z01@gmail.com>
2021-10-29 12:42:41 +0700

    sidebar + home + playmusic

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,4 +1,13 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-let navigation = $$('.sidebar__navigation-item')
+let navParent = $$('div.sidebar__navigation-link')
+navParent.forEach((item) => {
+    item.onclick = function () {
+        let listSubLink = this.nextElementSibling
+        let SubLink = listSubLink.querySelectorAll('.sidebar__navigation-subitem')
+        let active_height = SubLink[0].clientHeight * SubLink.length;
+        this.classList.toggle('show')
+        listSubLink.style.height = this.classList.contains('show') ? active_height + 'px' : 0
+    }
+})
